832ec42a1808bfaa62ef57e22db42aec6b4da52d
larryzodiac <evanmachale@gmail.com>
2019-01-12 16:30:25 +0000

    Updates

---
+++ precache-manifest.bce7bc2887ff8e2f61b9ca23c0d99ae5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokedex/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "69bab70dd00f4429b781",
+    "url": "/pokedex/static/js/runtime~main.69bab70d.js"
+  },
+  {
+    "revision": "dd96f45a69526259fffc",
+    "url": "/pokedex/static/js/main.dd96f45a.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/pokedex/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "dd96f45a69526259fffc",
+    "url": "/pokedex/static/css/main.e010d6ea.chunk.css"
+  },
+  {
+    "revision": "5d71012bc10619bf0438cdd180292723",
+    "url": "/pokedex/index.html"
+  }
+];
