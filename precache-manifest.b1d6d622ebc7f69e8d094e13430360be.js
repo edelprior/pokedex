c41163b73dc7ba0e3e767b96b0187b5b3dfbb0a1
larryzodiac <evanmachale@gmail.com>
2019-02-02 20:27:05 +0000

    Updates

--- precache-manifest.b1d6d622ebc7f69e8d094e13430360be.js
+++ precache-manifest.b1d6d622ebc7f69e8d094e13430360be.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/pokedex/static/js/runtime~main.69bab70d.js"
   },
   {
-    "revision": "ddb84ca48dce12d50d74",
-    "url": "/pokedex/static/js/main.ddb84ca4.chunk.js"
+    "revision": "d6667ea68918160c9243",
+    "url": "/pokedex/static/js/main.d6667ea6.chunk.js"
   },
   {
     "revision": "2aa37153e9578be54cb6",
     "url": "/pokedex/static/js/1.2aa37153.chunk.js"
   },
   {
-    "revision": "ddb84ca48dce12d50d74",
+    "revision": "d6667ea68918160c9243",
     "url": "/pokedex/static/css/main.b03b0c6a.chunk.css"
   },
   {
-    "revision": "210fae8bb3200aee166650df422dfd85",
+    "revision": "ca0b7f8586743a41a65bc4a5a5fd8bbc",
     "url": "/pokedex/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pokedex/precache-manifest.9b2912fc75503d26f5c93394af072537.js"
+  "/pokedex/precache-manifest.b1d6d622ebc7f69e8d094e13430360be.js"
 );
 
 workbox.clientsClaim();
